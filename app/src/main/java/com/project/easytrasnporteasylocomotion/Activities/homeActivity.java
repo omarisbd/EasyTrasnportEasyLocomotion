95810ceb0789c9ae27359eb9bbfee8fa4b55cd03
ibrahim <ibrodjabi@yahoo.fr>
2020-07-28 22:55:40 +0000

    up to  4th vid

---
+++ app/src/main/java/com/project/easytrasnporteasylocomotion/Activities/homeActivity.java
@@ -0,0 +1,16 @@
+package com.project.easytrasnporteasylocomotion.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.project.easytrasnporteasylocomotion.R;
+
+public class homeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
